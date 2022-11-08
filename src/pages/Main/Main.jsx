46767f835b4318b318f66ef2e4450d7793d23ac9
iwanfischer <ficicici1337@gmail.com>
2022-11-08 15:02:15 +0500

    small fixes

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -4,13 +4,13 @@ import Header from '../../components/Header/Header';
 import Tracks from '../../components/Tracks/Tracks';
 
 const Main = () => {
-  return (
-    <div className="main_container">
-      <Header />
-      <Tracks />
-      <Cards />
-    </div>
-  );
+    return (
+        <div className="main_container">
+            <Header/>
+            <Cards/>
+            <Tracks/>
+        </div>
+    );
 };
 
 export default Main;
