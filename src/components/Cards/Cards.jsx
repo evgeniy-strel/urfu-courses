e866ca316b9f81d296f7bae08c159e77a1c580bf
iwanfischer <ficicici1337@gmail.com>
2022-11-08 15:00:48 +0500

    rework, fix cards component

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -2,19 +2,45 @@ import React from "react";
 import "./cards.css";
 
 export default function Cards() {
-  return (
-    <div class="cards-list">
-      <div class="card">
-        <div class="card_title">
-          <p>Курс по хуйне 1</p>
-        </div>
-      </div>
+    return (
 
-      <div class="card">
-        <div class="card_title">
-          <p>Курс по хуйне 2</p>
+        <div className="cards">
+            <p className="cards_title">Список всех найденных курсов</p>
+            <div className="cards_list">
+
+                <div className="card">
+                    <div className="card_title">
+                        <p>Математические методы для разработчиков</p>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="card_title">
+                        <p>Экономические и гуманитарные аспекты информационных технологий</p>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="card_title">
+                        <p>Базовая архитектура программного обеспечения</p>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="card_title">
+                        <p>Профессиональный курс. Спецкурс 1</p>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="card_title">
+                        <p>Профессиональный курс. Спецкурс 2</p>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="card_title">
+                        <p>Математические методы для разработчиков 1</p>
+                    </div>
+                </div>
+
+
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
