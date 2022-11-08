6d513ff3f50c6057e4135097484dd94a4850ad47
iwanfischer <ficicici1337@gmail.com>
2022-11-08 14:59:51 +0500

    add tracks with working progress bar

--- src/components/Tracks/Tracks.jsx
+++ src/components/Tracks/Tracks.jsx
@@ -2,46 +2,72 @@ import React from "react";
 import "./tracks.css";
 
 export default function Tracks() {
-  return (
-    <div class="track-list">
-      <div class="track">
-        <div class="track_title">
-          <p>Трек по хуйне 1</p>
+    return (
+
+        <div className="tracks">
+            <p className="tracks_title">Список всех найденных треков с названием “методы” на 5 семестре:</p>
+            <div className="track_list">
+
+                <div className="track">
+                    <div className="track_title">
+                        <p>Методы оптимальных решений</p>
+                    </div>
+                    <div className="track_bar">
+                        <Progress done="70"/>
+                    </div>
+                </div>
+
+                <div className="track">
+                    <div className="track_title">
+                        <p>Методы оптимизации</p>
+                    </div>
+                    <div className="track_bar">
+                        <Progress done="70"/>
+                    </div>
+                </div>
+
+                <div className="track">
+                    <div className="track_title">
+                        <p>Методы операционных систем в разработке программного обеспечения (Онлайн, МИФИ, ОК)</p>
+                    </div>
+                    <div className="track_bar">
+                        <Progress done="70"/>
+                    </div>
+                </div>
+
+                <div className="track">
+                    <div className="track_title">
+                        <p>Правовые методы интеллектуальной собственности (Онлайн, ИТМО, ОК)</p>
+                    </div>
+                    <div className="track_bar">
+                        <Progress done="70"/>
+                    </div>
+                </div>
+
+            </div>
         </div>
-        <div className="track_bar">
-          <Progressbar progress="100" />
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
-const Progressbar = ({ progress }) => {
-  const Parentdiv = {
-    height: "15",
-    width: "100%",
-    borderRadius: 40,
-    margin: 50,
-    backgroundColor: "red",
-  };
-  const Childdiv = {
-    height: "100%",
-    width: `${progress}%`,
-    backgroundColor: "rgb(209, 238, 255)",
-    borderRadius: 40,
-    textAlign: "right",
-  };
+const Progress = ({done}) => {
+    const [style, setStyle] = React.useState({});
 
-  const progresstext = {
-    padding: 10,
-    color: "white",
-  };
+    setTimeout(() => {
+        const newStyle = {
+            opacity: 1,
+            width: `${done}%`
+        }
 
-  return (
-    <div style={Parentdiv}>
-      <div style={Childdiv}>
-        <span style={progresstext}>{`${progress}%`}</span>
-      </div>
-    </div>
-  );
+        setStyle(newStyle);
+    }, 1000);
+
+    return (
+        <div className="progress">
+            <div className="progress-done" style={style}>
+                {done}%
+            </div>
+        </div>
+
+    )
+
 };
