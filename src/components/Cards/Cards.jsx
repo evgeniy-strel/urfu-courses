fbb0ad83155fd5de29d08a5fb24c454685316aa1
iwanfischer <ficicici1337@gmail.com>
2022-11-10 21:04:18 +0500

    add link to cards + small changes

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -2,49 +2,53 @@ import React from 'react';
 import './cards.css';
 
 export default function Cards() {
-  return (
-    <div className='cards'>
-      <p className='cards_title'>Список всех найденных курсов:</p>
-      <div className='cards_list'>
-        <div className='card'>
-          <div className='card_background'></div>
-          <div className='card_title'>
-            <p>Математические методы для разработчиков</p>
-          </div>
+    return (
+        <div className='cards'>
+            <p className='cards_title'>Список всех найденных курсов:</p>
+            <div className='cards_list'>
+                <div className='card'>
+                    <div className='card_background'></div>
+                    <p className='card_title'>Математические методы для разработчиков</p>
+                    <a href="#">
+                        <span className="link"></span>
+                    </a>
+                </div>
+                <div className='card'>
+                    <div className='card_background'></div>
+                    <p className='card_title'>Экономические и гуманитарные аспекты информационных технологий</p>
+                    <a href="#">
+                        <span className="link"></span>
+                    </a>
+                </div>
+                <div className='card'>
+                    <div className='card_background'></div>
+                    <p className='card_title'>Базовая архитектура программного обеспечения</p>
+                    <a href="#">
+                        <span className="link"></span>
+                    </a>
+                </div>
+                <div className='card'>
+                    <div className='card_background'></div>
+                    <p className='card_title'>Профессиональный курс. Спецкурс 1</p>
+                    <a href="#">
+                        <span className="link"></span>
+                    </a>
+                </div>
+                <div className='card'>
+                    <div className='card_background'></div>
+                    <p className='card_title'>Профессиональный курс. Спецкурс 2</p>
+                    <a href="#">
+                        <span className="link"></span>
+                    </a>
+                </div>
+                <div className='card'>
+                    <div className='card_background'></div>
+                    <p className='card_title'>Математические методы для разработчиков 1</p>
+                    <a href="#">
+                        <span className="link"></span>
+                    </a>
+                </div>
+            </div>
         </div>
-        <div className='card'>
-          <div className='card_background'></div>
-          <div className='card_title'>
-            <p>
-              Экономические и гуманитарные аспекты информационных технологий
-            </p>
-          </div>
-        </div>
-        <div className='card'>
-          <div className='card_background'></div>
-          <div className='card_title'>
-            <p>Базовая архитектура программного обеспечения</p>
-          </div>
-        </div>
-        <div className='card'>
-          <div className='card_background'></div>
-          <div className='card_title'>
-            <p>Профессиональный курс. Спецкурс 1</p>
-          </div>
-        </div>
-        <div className='card'>
-          <div className='card_background'></div>
-          <div className='card_title'>
-            <p>Профессиональный курс. Спецкурс 2</p>
-          </div>
-        </div>
-        <div className='card'>
-          <div className='card_background'></div>
-          <div className='card_title'>
-            <p>Математические методы для разработчиков 1</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
